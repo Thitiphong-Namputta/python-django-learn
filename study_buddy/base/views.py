614f75e54ec496c6ad011f06db5f50f2c0696c70
big <thitiphong479big@hotmail.com>
2025-11-09 20:59:07 +0700

    migration and add models rooms topic message

--- study_buddy/base/views.py
+++ study_buddy/base/views.py
@@ -1,24 +1,16 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {"id": 1, "name": "Lets learn Python!"},
-    {"id": 2, "name": "Design with me"},
-    {"id": 3, "name": "Frontend developers"},
-]
-
 
 def home(request):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
-    room = None
-    for r in rooms:
-        if r["id"] == int(pk):
-            room = r
-            break
+    room = Room.objects.get(id=pk)
     context = {"room": room}
-    return render(request, "base/room.html" , context)
+    return render(request, "base/room.html", context)
